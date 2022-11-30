66280e5a4ceb130928fd607ddd4ff5afb91bbf34
Madina Mel <wolfi251199@gmail.com>
2022-12-01 00:06:55 +0100

    Lesson 2 Triggers component

---
+++ Lesson_02/vocabulary_cards/src/components/Triggers/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import s from './index.module.css'
+
+export default function Triggers() {
+  return (
+    <div className={s.triggers}>
+        <button>ENG</button>
+        <button>RUS</button>
+    </div>
+  )
+}
