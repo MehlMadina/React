595ca78f66c30e96bce1d7937cb3354d00735ee4
Madina Mel <wolfi251199@gmail.com>
2023-03-12 19:27:35 +0100

    Project completed

--- Lesson_05/posts_master/src/components/Comments/index.jsx
+++ Lesson_05/posts_master/src/components/Comments/index.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import s from "./style.module.css";
+import { Context } from '../../context';
 
-export default function Comments({ text }) {
-  return <p className={s.comment_item}>{text}</p>;
+export default function Comments({id, text, post_id}) {
+
+const { delete_comment } = useContext(Context);
+
+  return <p className={s.comment_item} onClick={() => delete_comment(post_id, id)}>{text}</p>;
 }
--- Lesson_05/posts_master/src/components/CommentsContainer/index.jsx
+++ Lesson_05/posts_master/src/components/CommentsContainer/index.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
-import AddCommentForm from '../AddCommentForm'
-import Comments from '../Comments'
+import React from "react";
+import AddCommentForm from "../AddCommentForm";
+import Comments from "../Comments";
 
-
-
-export default function CommentsContainer({comments, add_comment, id}) {
-
-
+export default function CommentsContainer({ comments, add_comment, id }) {
   return (
     <div>
-        {comments.map(el => <Comments key={el.id} {...el}/>)}
-        <AddCommentForm add_comment={add_comment} id={id}/>
+      {comments.map((el) => (
+        <Comments key={el.id} {...el} post_id={id} />
+      ))}
+      <AddCommentForm add_comment={add_comment} id={id} />
     </div>
-  )
+  );
 }
