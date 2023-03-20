b8b45c2f418b49972d8285b6419375d023b31f33
Madina Mel <wolfi251199@gmail.com>
2023-03-20 15:50:33 +0100

    Language cards added

--- Lesson_12/redux/src/components/App/index.jsx
+++ Lesson_12/redux/src/components/App/index.jsx
@@ -1,10 +1,12 @@
 import Products from "../Products";
-
+import LanguageCards from '../LanguageCards';
 
 function App() {
   return (
     <div >
       <Products />
+      <br/>
+      <LanguageCards />
     </div>
   );
 }
