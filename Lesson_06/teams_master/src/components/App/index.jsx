f3624c14d8a3c755faf9699c4d58949e06afaaca
Madina Mel <wolfi251199@gmail.com>
2023-03-20 19:42:32 +0100

    Added localStorage

--- Lesson_06/teams_master/src/components/App/index.jsx
+++ Lesson_06/teams_master/src/components/App/index.jsx
@@ -2,12 +2,24 @@ import ConfigurationsPages from "../../pages/ConfigurationsPages";
 import TeamsPages from "../../pages/TeamsPages";
 import Nav from "../Nav";
 import { Routes, Route } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Context } from '../../context'
 
 function App() {
   const [teams, setTeams] = useState([]);
   const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const teams = JSON.parse(localStorage.getItem('teams'));
+    const users = JSON.parse(localStorage.getItem('users'));
+    setTeams(teams);
+    setUsers(users);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('teams', JSON.stringify(teams));
+    localStorage.setItem('users', JSON.stringify(users));
+  }, [teams, users])
 
   const add_team = (value) => {
     setTeams([...teams, { value, label: value }]);
@@ -20,7 +32,7 @@ function App() {
       {
         id: Date.now(),
         name,
-        team,
+        team
       },
     ]);
   };
